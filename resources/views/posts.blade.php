9ef72ce45c8d0e7be64a6849c8c8638359615e3c
Charles <charlesmariga37@gmail.com>
2023-01-15 01:13:32 +0300

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/app.css">
+
+    <title>My blog</title>
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post/my-first-post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In esse, odio natus corrupti soluta quam alias ipsa
+            illum earum minima perspiciatis magnam explicabo eius, architecto, veniam animi quia labore consequuntur
+            vero fugit repellat aspernatur. Autem assumenda debitis, facere quod alias nihil. Dignissimos quod rem odio
+            necessitatibus accusamus maxime blanditiis doloribus rerum magnam quisquam, possimus eos unde aliquid.
+            Quibusdam quo nam laboriosam qui ut facilis reprehenderit aliquam quia facere. Quam a, in tenetur rem ipsam
+            animi voluptates illo exercitationem cum esse eveniet veniam at ex placeat? Asperiores culpa laboriosam aut
+            exercitationem, tempore voluptates, ab, corporis veniam error vel sapiente maxime autem.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post/my-second-post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In esse, odio natus corrupti soluta quam alias ipsa
+            illum earum minima perspiciatis magnam explicabo eius, architecto, veniam animi quia labore consequuntur
+            vero fugit repellat aspernatur. Autem assumenda debitis, facere quod alias nihil. Dignissimos quod rem odio
+            necessitatibus accusamus maxime blanditiis doloribus rerum magnam quisquam, possimus eos unde aliquid.
+            Quibusdam quo nam laboriosam qui ut facilis reprehenderit aliquam quia facere. Quam a, in tenetur rem ipsam
+            animi voluptates illo exercitationem cum esse eveniet veniam at ex placeat? Asperiores culpa laboriosam aut
+            exercitationem, tempore voluptates, ab, corporis veniam error vel sapiente maxime autem.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post/my-third-post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In esse, odio natus corrupti soluta quam alias ipsa
+            illum earum minima perspiciatis magnam explicabo eius, architecto, veniam animi quia labore consequuntur
+            vero fugit repellat aspernatur. Autem assumenda debitis, facere quod alias nihil. Dignissimos quod rem odio
+            necessitatibus accusamus maxime blanditiis doloribus rerum magnam quisquam, possimus eos unde aliquid.
+            Quibusdam quo nam laboriosam qui ut facilis reprehenderit aliquam quia facere. Quam a, in tenetur rem ipsam
+            animi voluptates illo exercitationem cum esse eveniet veniam at ex placeat? Asperiores culpa laboriosam aut
+            exercitationem, tempore voluptates, ab, corporis veniam error vel sapiente maxime autem.</p>
+    </article>
+</body>
+
+</html>
