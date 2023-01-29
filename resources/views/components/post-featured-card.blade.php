bf31658feb3e73ecda1517c76fc376a3fcdb5d61
Charles <charlesmariga37@gmail.com>
2023-01-29 13:26:12 +0300

    fix: quick tweaks and cleanup

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -30,22 +30,19 @@
                    </div>
                </header>
 
-               <div class="text-sm mt-2">
-                   <p>
-                       {{ $post->excerpt }}
-                   </p>
+               <div class="text-sm mt-2 space-y-4">
+                   {!! $post->excerpt !!}
                </div>
 
-               <footer class="flex justify-between items-center mt-8">
+               <footer class="flex justify-between mt-8 flex-1 items-end">
                    <div class="flex items-center text-sm">
                        <img src="/images/lary-avatar.svg" alt="Lary avatar">
                        <div class="ml-3">
                            <h5 class="font-bold">{{ $post->author->name }}</h5>
-                           <h6>Mascot at Laracasts</h6>
                        </div>
                    </div>
 
-                   <div class="hidden lg:block">
+                   <div class="mb-4">
                        <a href="/posts/{{ $post->slug }}"
                            class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Read
                            More</a>
