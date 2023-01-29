8b957411e6c335f33d704e8f0d3de659b35b264b
Charles <charlesmariga37@gmail.com>
2023-01-29 18:20:26 +0300

    fix: pagination

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,9 +7,11 @@
             <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" />
         </button>
     </x-slot>
-    <x-dropdown-item href="/" active="{{ !request('category') }}">All</x-dropdown-item>
+    <x-dropdown-item href="/?{{ http_build_query(request()->except('page')) }}" active="{{ !request('category') }}">All
+    </x-dropdown-item>
     @foreach ($categories as $category)
-        <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+        <x-dropdown-item
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
             active="{{ request('category') === $category->slug ? true : false }}">
             {{ ucwords($category->name) }}</x-dropdown-item>
     @endforeach
